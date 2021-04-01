02145ebb27604f6973dc4753f4f52a9a9673a0fc
AnnaLasmane <anna.gravere@gmail.com>
2021-04-01 20:14:24 +0300

    gg

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function js_style(){
+const textElement = document.getElementById('JavaScript Exercises - w3resource')
+textElement.style.fontSize = '36px';
+textElement.style.color = 'purple';
+textElement.style.fontFamily = 'veranda';
+}
